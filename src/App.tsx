70908adbefff0f5b2f5312cefa7804e2eaa3df74
tanmay kumar chaurasia <130184741+TanmayChaurasia24@users.noreply.github.com>
2024-09-07 16:42:28 +0530

    written logic for ws server and client

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [socket, setsocket] = useState<null | WebSocket>(null);
+  const [LatestMessage,setLatestMessage] = useState("");
+  const [sendmessage,setsendmessage] = useState("");
+  useEffect(() => {
+    const socket = new WebSocket("ws://localhost:8080");
+
+    socket.onopen = () => {
+      console.log("connected");
+      setsocket(socket);
+    };
+
+    socket.onmessage = (message) => {
+      console.log("received message", message.data);
+      setLatestMessage(message.data);
+    };
+  }, []);
+
+  if (!socket) {
+    return <div>Connecting To Web Socket Server...</div>;
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <input onChange={(e) => {
+        setsendmessage(e.target.value)
+      }}/>
+      <button
+        onClick={() => {
+          socket.send(sendmessage);
+        }}
+      >
+        Send
+      </button>
+      {LatestMessage}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
